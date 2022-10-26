8b20920a84414ecfa0a7fdbc8eacd0b3a80923c9
Usman-turing <muhammad.usman@turing.com>
2022-10-26 17:53:33 +0000

    addmig model file

--- Python/scripts/Short Project Template/notebooks/settings.py
+++ Python/scripts/Short Project Template/notebooks/settings.py
@@ -2,7 +2,7 @@
 
 query_config_path = 'query_config.yaml'
 
-finetuned_model_path = "../model/model-name.pickle"
+finetuned_model_path = "../model/fine_tune_model.pickle"
 
 raw_dataset_path = '../data/raw_dataset.pickle'
 
