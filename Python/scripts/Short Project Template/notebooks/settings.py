e1f04b8d4650a7d215c0aafb198ba8eed71ec388
Usman-turing <muhammad.usman@turing.com>
2022-10-26 17:25:06 +0000

    Template for short Projects

---
+++ Python/scripts/Short Project Template/notebooks/settings.py
@@ -0,0 +1,9 @@
+"""Contains constants and settings"""
+
+query_config_path = 'query_config.yaml'
+
+finetuned_model_path = "../model/model-name.pickle"
+
+raw_dataset_path = '../data/raw_dataset.pickle'
+
+processed_dataset_path = '../data/processed_dataset.pickle'
