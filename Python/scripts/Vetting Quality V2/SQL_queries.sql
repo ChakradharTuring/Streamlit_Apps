895da1febf4bcf11f5fa3bb931511c0156645d84
Usman-turing <muhammad.usman@turing.com>
2023-04-21 06:26:58 +0500

    query update

--- Python/scripts/Vetting Quality V2/SQL_queries.sql
+++ Python/scripts/Vetting Quality V2/SQL_queries.sql
@@ -4,3 +4,13 @@ pd.read_gbq('CREATE TABLE `turing-dev-337819.pdsa.question_quality` (`Problem_ID
             `Point_biserial_correlation` FLOAT64, `P_value` FLOAT64,\
             `Distractor_Efficiency` FLOAT64,\
             `Question_Type` string, `Options` INT64, `Updated_date_run` Timestamp )')
+
+
+
+
+pd.read_gbq('CREATE TABLE `turing-dev-337819.pdsa.question_quality` (`Problem_ID` INT64, `IRs` FLOAT64, `DCs` FLOAT64,\
+            `DC_IR_Difference` FLOAT64, `Version` string, `MCQ_Name` string,\
+            `MCQ_ID` INT64, `Attempts` INT64, `Pass_pct` FLOAT64, `Launch_Date` Timestamp,\
+            `Point_biserial_correlation` FLOAT64, `P_value` FLOAT64,\
+            `Distractor_Efficiency` FLOAT64,\
+            `Question_Type` string, `Options` INT64, `Updated_date_run` Timestamp )')
